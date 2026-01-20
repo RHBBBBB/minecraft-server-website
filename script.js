f3d2855167091c0fa9c39ceeeba9bbce5b65654d
RHBBBBB <r.hocini0305@gmail.com>
2026-01-20 18:29:40 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const elements = document.querySelectorAll(".fade-up");
+
+const reveal = () => {
+  elements.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add("visible");
+    }
+  });
+};
+
+window.addEventListener("scroll", reveal);
+reveal();
